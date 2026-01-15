d1dca7ee0682339a345ed2768a32d429a2419ce4
The_aura2005 <wanjalalaura0@gmail.com>
2026-01-15 21:33:32 +0300

    Update admin, remove old CSS, add new admin CSS

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -9,4 +9,25 @@
             </div>
         </div>
         <!--.Main Section Ends Here -->
+          <!--Button To Add Admin -->
+          <a href="#" class="btn-primary">Add Admin</a>
+          </br></br><br>
+          <table class="tbl-full">
+            <tr>
+                <th>S.N.</th>
+                <th>Full Name</th>
+                <th>Username</th>
+                <th>Actions</th>
+            </tr>
+            <tr>
+                <td>1.</td>
+                <td>Laura Wanjala</td>
+                <td>Laura</td>
+                <td>
+                    <a href="#" class="btn-secondary">Update Admin</a>
+                    <a href="#" class="btn-secondary">Delete Admin</a>
+
+                </td>
+            </tr>
+          </table>
         <?php include('partials/footer.php'); ?>
